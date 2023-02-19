583805cbab9c8e221b86df6f5ed708f5cc653516
Sangho <segoviamin@gmail.com>
2023-02-19 16:52:39 +0900

    day06

---
+++ src/main/scala/aoc/Day06.scala
@@ -0,0 +1,27 @@
+package aoc
+
+import scala.collection.mutable
+import scala.io.Source
+
+object Day06 extends App {
+  private val expectLength = 14
+
+  val filename = "src/main/scala/aoc/Day06.input"
+  val line = Source.fromFile(filename).getLines().toList(0)
+  def findUniqueIndex(line: Array[Char], queue: mutable.Queue[Char], index: Int): Int = {
+    if (queue.size == expectLength) {
+      return index
+    }
+
+    if (queue.contains(line(0))) {
+      queue.removeHeadWhile(p => p != line(0))
+      queue.remove(0)
+    }
+    queue.addOne(line(0))
+
+    findUniqueIndex(line.splitAt(1)._2, queue, index + 1)
+  }
+
+  val result = findUniqueIndex(line.toCharArray, new mutable.Queue[Char], 0)
+  println(result)
+}
---
+++ src/test/scala/aoc/Day06Test.scala
@@ -0,0 +1,27 @@
+package aoc
+
+import aoc.Day06.findUniqueIndex
+import hedgehog.munit.HedgehogSuite
+
+import scala.collection.mutable
+
+class Day06Test extends HedgehogSuite {
+
+  val inputs = """bvwbjplbgvbhsrlpgdmjqwftvncz
+                 |nppdvjthqldpwncqszvftbrmjlhg
+                 |nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
+                 |zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
+                 |""".stripMargin.split("\n")
+
+  test("test uniq seq index") {
+    inputs.foreach(input => {
+      val inputChs = input.toCharArray
+      val ret = findUniqueIndex(inputChs, new mutable.Queue[Char], 0)
+      println(ret)
+    })
+  }
+}
+
+/**
+ * 배운점. String 에는 toCharArray 라는 편한 method 가 있었음
+ */
