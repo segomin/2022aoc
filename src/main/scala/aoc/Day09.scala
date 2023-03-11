da9332124f06ab3c3f20a237ef3e7344def26989
민상호 <shmin@seowoninfo.com>
2023-03-11 16:27:10 +0900

    day09 part2

--- src/main/scala/aoc/Day09.scala
+++ src/main/scala/aoc/Day09.scala
@@ -7,95 +7,97 @@ import scala.io.Source
 object Day09 extends App {
   val filename = "src/main/scala/aoc/Day09.input"
   val lines = Source.fromFile(filename).getLines()
-  val river = River.of(lines.toArray)
-  val part1 = river.countTailVisit()
+  val inputs = lines.toArray
 
+  val part1 = new River(2).moveAll(inputs).tailVisitCount()
   println(part1)
+
+  val part2 = new River(10).moveAll(inputs).tailVisitCount()
+  println(part2)
 }
 
 
-class River(val river: mutable.HashSet[Knot], var head: Knot, var tail: Knot) {
+class River(ropeSize: Int) {
+
+  var knots: Array[Knot] = Array.fill[Knot](ropeSize)(Knot(0, 0))
+  val river: mutable.HashSet[Knot] = mutable.HashSet()
   def moveAll(inputs: Array[String]) = {
     inputs.foreach(line => {
       val console = line.split(" ")
-      console match {
-        case Array(direction, step) => moveHead(direction, step.toInt)
+      val (direction: String, step: Int) = (console(0), console(1).toInt)
+
+      def moveTail(head: Knot, tail: Knot, mark: Knot => Unit): Knot = {
+        @tailrec
+        def followTail(head: Knot, tail: Knot): Knot = {
+          mark(tail)
+
+          if (tail.isNear(head)) {
+            return tail
+          }
+
+          val moved = tail.moveToward(head)
+          followTail(head, moved)
+        }
+
+        followTail(head, tail)
+      }
+
+      @tailrec
+      def iterateMove(head: Knot, tails: Array[Knot], acc: Array[Knot]): Array[Knot] = {
+        tails match {
+          case Array(last) =>
+            val moved = moveTail(head, last, knot => river.add(knot))
+            iterateMove(moved, tails.tail, acc :+ moved);
+          case Array(one, _*) =>
+            val moved = moveTail(head, one, _ => ())
+            iterateMove(moved, tails.tail, acc :+ moved);
+          case _ => acc
+        }
+      }
+
+      def moveSteps(direction: String): Unit = {
+        val head = direction match {
+          case "R" => knots.head.rightTo()
+          case "L" => knots.head.leftTo()
+          case "U" => knots.head.upTo()
+          case "D" => knots.head.downTo()
+        }
+        val newKnots = iterateMove(head, knots.tail, Array(head))
+        knots = newKnots
+      }
+
+      // part 1 에서 head 를 한번에 옮긴 후 tail 을 이동해 보았으나,
+      // tail 이 길어질 경우 이렇게는 안되서 하나씩 옮기는 방식으로 변경함
+      for(_ <- 0 until step) {
+        moveSteps(direction)
       }
     })
     this
   }
 
-  def countTailVisit() = river.size
-
-  def moveHead(direction: String, step: Int): Unit = {
-    head = direction match {
-      case "R" => head.rightTo(step)
-      case "L" => head.leftTo(step)
-      case "U" => head.upTo(step)
-      case "D" => head.downTo(step)
-    };
-    tail.followAndMark(head, river)
-    tail.followAndMark(head, river)
-    tail.followAndMark(head, river)
-    tail.followAndMark(head, river)
-  }
-}
-
-object River {
-  def of(inputs: Array[String]): River = {
-    val tailBoard = new mutable.HashSet[Knot]()
-    val tail = Knot(0, 0)
-    val head = Knot(0, 0)
-    new River(tailBoard, head, tail).moveAll(inputs)
-  }
+  def tailVisitCount() = river.size
 }
 
 case class Knot(x: Int, y: Int) {
-  def moveToward(head: Knot): Knot = {
-    if (head.x == x) {
-      Knot(x, if (head.y > y) y + 1 else y - 1)
-    } else if (head.y == y) {
-      Knot(if (head.x > x) x + 1 else x - 1, y)
+  def moveToward(target: Knot): Knot = {
+    if (notInLine(target))
+      return Knot(x + (target.x - x).sign, y + (target.y - y).sign)
+
+    if (target.x == x) {
+      Knot(x, y + (target.y - y).sign)
+    } else if (target.y == y) {
+      Knot(x + (target.x - x).sign, y)
     } else throw new RuntimeException("")
   }
 
-  def moveToLine(head: Knot): Knot = {
-    if (Math.abs(head.x - x) == 1) {
-      Knot(head.x, y)
-    } else if (Math.abs(head.y - y) == 1){
-      Knot(x, head.y)
-    } else throw new RuntimeException("")
-  }
+  def notInLine(target: Knot): Boolean = target.x != x && target.y != y
 
-  def notInLine(head: Knot): Boolean = head.x != x && head.y != y
-
-  def isNear(head: Knot): Boolean = {
-    if (Math.abs(x - head.x) < 2 && Math.abs(y - head.y) < 2) true
+  def isNear(target: Knot): Boolean = {
+    if ((target.x - x).abs < 2 && (target.y - y).abs < 2) true
     else false
   }
-
-  def followAndMark(head: Knot, board: mutable.HashSet[Knot]): Knot = {
-
-    @tailrec
-    def followAndMark(head: Knot, tail: Knot): Knot = {
-      board.add(tail)
-
-      if (tail.isNear(head)) {
-        return tail
-      }
-
-      var tail2 = tail
-      if (tail.notInLine(head))
-        tail2 = tail.moveToLine(head)
-
-      followAndMark(head, tail2.moveToward(head))
-    }
-
-    followAndMark(head, this)
-  }
-
-  def rightTo(step: Int): Knot = Knot(x + step, y)
-  def leftTo(step: Int): Knot = Knot(x - step, y)
-  def upTo(step: Int): Knot = Knot(x, y + step)
-  def downTo(step: Int): Knot = Knot(x, y - step)
+  def rightTo(step: Int = 1): Knot = Knot(x + step, y)
+  def leftTo(step: Int = 1): Knot = Knot(x - step, y)
+  def upTo(step: Int = 1): Knot = Knot(x, y + step)
+  def downTo(step: Int = 1): Knot = Knot(x, y - step)
 }
--- src/test/scala/aoc/Day09Test.scala
+++ src/test/scala/aoc/Day09Test.scala
@@ -12,15 +12,40 @@ class Day09Test extends HedgehogSuite {
       |D 1
       |L 5
       |R 2""".stripMargin.split("\n")
+  val inputs2 =
+    """R 5
+      |U 8
+      |L 8
+      |D 3
+      |R 17
+      |D 10
+      |L 25
+      |U 20""".stripMargin.split("\n")
 
 
-  test("visit short part1") {
-    val river = River.of(inputs)
-    val count = river.countTailVisit()
+  test("tail visit count part1") {
+    val river = new River(2).moveAll(inputs)
+    val count = river.tailVisitCount()
+
     println(count)
+    withMunitAssertions { assertions =>
+      assertions.assertEquals(count, 13)
+    }
   }
 
-  test("visit long part2") {
+  test("tail visit count part2") {
+    val river = new River(10).moveAll(inputs2)
+    val count = river.tailVisitCount()
+
+    println(count)
+    withMunitAssertions { assertions =>
+      assertions.assertEquals(count, 36)
+    }
   }
 }
 
+/**
+ * 배운점
+ * Math.abs(num) -> (num).abs 도 가능
+ * array pattern-match  `_*` 로 나머지 표현 가능
+ */
